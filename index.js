47ac2d11080381a2a232fc2b3b152ded1e856d88
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2022-10-16 22:53:28 +0800

    2nd commit with branch

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const http = require('http').Server(app);
 
-
 //path routes
 const users = require('./routes/users')(router);
 
@@ -24,7 +23,6 @@ mongoose.connect(config.uri, config.options, (err) => {
     }
 });
 
-
 app.use(cors())
 
 //body-parser built in express middleware
@@ -35,7 +33,6 @@ app.use(express.json());
 app.use(express.static(__dirname + '/app/dist/'));
 app.use('/images', express.static(path.join(__dirname, './images')));
 app.use('/upload', express.static(path.join(__dirname, './upload')));
-
 
 //api routes
 app.use('/users', users);
