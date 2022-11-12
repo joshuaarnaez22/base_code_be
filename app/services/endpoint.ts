15d69e946e86771f6ff7fedd31d7c8d10c80bdf8
Joshua V Arnaez <jarnaez@yondu.com>
2022-11-12 21:10:17 +0800

    ...

--- app/services/endpoint.ts
+++
@@ -1,5 +0,0 @@
-const LOCALDEV = 'holy-infant.herokuapp.com';
-export const addUser = `${LOCALDEV}/users/addUser`;
-export const getAllUser = `${LOCALDEV}/users/getAllUser`;
-export const updateUser = `${LOCALDEV}/users/updateUser`;
-export const deleteUser = `${LOCALDEV}/users/deleteUser`;
