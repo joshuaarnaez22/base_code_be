5abd75bb9cc6d4a359849caa1219795838cc038f
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2022-11-20 22:06:56 +0800

    restor login

--- routes/authentication.js
+++ routes/authentication.js
@@ -129,41 +129,41 @@ module.exports = (router) => {
               } else {
                 //user found compare the password
 
-                  bcrypt.compare(req.body.password, user.password).then(function(result) {
-                  if (!result) {
-                  res.json({ success: false, message: 'Password is incorrect' })
-                  } else {
-                  const token = jwt.sign({ userID: user._id }, config.secret, { expiresIn: '24h' });
-                  res.json({ success: true, message: 'Password is Correct', token: token, user: { username: user.username }, userToken: user.username, role: user.role },)
-                  }
-                  }, function(err) {
-                  console.log(err); // Error: "It broke"
-                  }); 
+                  // bcrypt.compare(req.body.password, user.password).then(function(result) {
+                  // if (!result) {
+                  // res.json({ success: false, message: 'Password is incorrect' })
+                  // } else {
+                  // const token = jwt.sign({ userID: user._id }, config.secret, { expiresIn: '24h' });
+                  // res.json({ success: true, message: 'Password is Correct', token: token, user: { username: user.username }, userToken: user.username, role: user.role },)
+                  // }
+                  // }, function(err) {
+                  // console.log(err); // Error: "It broke"
+                  // }); 
 
 
-                // const validPassword = user.comparePassword(req.body.password);
-                // if (!validPassword) {
-                //   res.json({
-                //     success: false,
-                //     message: "Password is incorrect",
-                //   });
-                // } else {
-                //   const token = jwt.sign(
-                //     { userID: user._id, role: user.role },
-                //     config.secret,
-                //     {
-                //       expiresIn: "24h",
-                //     }
-                //   );
-                //   res.json({
-                //     success: true,
-                //     message: "Password is Correct",
-                //     token: token,
-                //     user: { username: user.username },
-                //     userToken: user.username,
-                //     role: user.role,
-                //   });
-                // }
+                const validPassword = user.comparePassword(req.body.password);
+                if (!validPassword) {
+                  res.json({
+                    success: false,
+                    message: "Password is incorrect",
+                  });
+                } else {
+                  const token = jwt.sign(
+                    { userID: user._id, role: user.role },
+                    config.secret,
+                    {
+                      expiresIn: "24h",
+                    }
+                  );
+                  res.json({
+                    success: true,
+                    message: "Password is Correct",
+                    token: token,
+                    user: { username: user.username },
+                    userToken: user.username,
+                    role: user.role,
+                  });
+                }
 
 
 
