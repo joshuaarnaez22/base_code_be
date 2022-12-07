c53f55222d964d7c81eecf36e600b3a36d212381
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2022-12-08 00:08:09 +0800

    aggregate getallmomoitringsss

--- routes/authentication.js
+++ routes/authentication.js
@@ -134,7 +134,7 @@ module.exports = (router) => {
                   res.json({ success: false, message: 'Password is incorrect' })
                   } else {
                     const token = jwt.sign(
-                      { userID: user.id, role: user.role },
+                      { userID: user._id, role: user.role, userId : user.id },
                       config.secret,
                       {
                         expiresIn: "24h",
