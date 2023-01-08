00d6e10888b8f1b7683b102576981784babc1e75
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2023-01-08 20:54:41 +0800

    dsfdfd

--- routes/users.js
+++ routes/users.js
@@ -340,7 +340,7 @@ module.exports = (router) => {
   router.get('/getAllVolunteer', (req, res) => {
 
     // Search database for all blog posts
-    Users.find({ deleted: false,  "role": "volunteer", }, { _id: 1, email: 1, username: 1, role: 1, status: 1 }, (err, user) => {
+    User.find({ deleted: false,  "role": "volunteer", }, { _id: 1, email: 1, username: 1, role: 1, status: 1 }, (err, user) => {
         // Check if error was found or not
         if (err) {
             res.json({ success: false, message: err }); // Return error message
@@ -359,7 +359,7 @@ module.exports = (router) => {
 router.get('/getTotalVolunteer', (req, res) => {
 
     // Search database for all blog posts
-    Volunteer.countDocuments({ deleted: false,  "role": "volunteer", }, (err, volunteer) => {
+    User.countDocuments({ deleted: false,  "role": "volunteer", }, (err, volunteer) => {
         // Check if error was found or not
         if (err) {
             res.json({ success: false, message: err }); // Return error message
