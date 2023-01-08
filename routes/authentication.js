e350051b62587f4ff8f3ea3a1711e1966d50bd14
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2023-01-08 21:49:03 +0800

    fasdfsadfdsa

--- routes/authentication.js
+++ routes/authentication.js
@@ -136,7 +136,7 @@ module.exports = (router) => {
 
                     const fullname = `${user.firstname} ${user.lastname}`
                     const token = jwt.sign(
-                      { userID: user._id, role: user.role, userId : user.id, fullname : fullname, lastname : user.lastname, email : user.email },
+                      { userID: user._id, role: user.role, userId : user.id, fullname : fullname, lastname : user.lastname,  firstname : user.firstname, username: user.username, email : user.email },
                       config.secret,
                       {
                         expiresIn: "24h",
