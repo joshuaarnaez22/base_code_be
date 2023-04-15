80a79c65fcd84695d052abb566d43cf91a48edb7
mmeguizo <mark.meguizo@gmail>
2023-04-16 02:03:52 +0800

    pushers

---
+++ models/monitoring.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+let bcrypt = require('bcryptjs');
+const { Schema } = mongoose;
+
+
+const monitoringSchema = new Schema({
+      id :  { type: String, required: true,  },
+      addedby :  { type: String, required: true,  },
+      orphan_id :  { type: String, required: true,  },
+      status :  { type: String, default : 'active'  },
+      date :  { type: Date,  }, 
+      deleted :  { type: Boolean, default : false  },
+      education : { type: String,  }, 
+      daily_health : [
+
+        { value : { type: String,  },
+          label : { type: String,  }
+        }
+
+      ],
+      chores : [
+
+        { value : { type: String,  },
+          label : { type: String,  }
+        }
+
+      ], 
+      action : { type: String,  },
+      meal : { type: String,  }
+  });
+
+  
+module.exports = mongoose.model('Monitoring', monitoringSchema);
+
